fe17e20a67e25cfba2c33bcacc18e34961903eab
Vladyslav <v1adem@ukr.net>
2023-11-16 16:36:37 +0200

    Product.java added

--- src/main/java/org/example/lab_7/Product.java
+++ src/main/java/org/example/lab_7/Product.java
@@ -1,4 +1,16 @@
 package org.example.lab_7;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
 public class Product {
+    private final Integer id;
+    private final String name;
+    private double price;
+    private int stock;
 }
